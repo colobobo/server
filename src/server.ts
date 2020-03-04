9fbd01cdb68f0beca0f6ca9d9e101496536a4c6e
Eronne <erwann.letue@gmail.com>
2020-03-04 16:48:49 +0100

    chore(*): Add cors

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,10 @@ import { Socket } from 'socket.io';
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const cors = require('cors');
 
 app.set('port', process.env.PORT || 3001);
+app.use(cors());
 
 io.on('connection', function(socket: Socket) {
   console.log('User connected:', socket.id);
